b101731bd369c76ccf42a9fc03c613f5b9dfc1ac
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-15 23:40:30 +0200

    websecurity configurations

--- src/main/java/com/kibcode/tweety/config/OAuth2AuthorizationServerConfigurer.java
+++ src/main/java/com/kibcode/tweety/config/OAuth2AuthorizationServerConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,11 +21,16 @@ public class OAuth2AuthorizationServerConfigurer extends AuthorizationServerConf
     }
 
     @Override
-    public void configure (ClientDetailsServiceConfigurer clients) throws Exception {
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("angularjsapp")
                 .secret("{noop}angularjs123")
                 .authorizedGrantTypes("password")
                 .scopes("read", "write");
+    }
+
+    @Override
+    public void configure (AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManagerBean);
     }
 }
---
+++ src/main/java/com/kibcode/tweety/config/WebSecurity.java
@@ -0,0 +1,28 @@
+package com.kibcode.tweety.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+}
